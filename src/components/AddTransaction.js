c8825a4ce5ead4b3b8b7196db0f4f1df1912eff7
cedric-winbush <cawinbushjr@gmail.com>
2020-08-16 23:02:54 -0500

    adds transaction reducer - finished -- time to build more withit

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,32 @@
-import React, { useState} from "react";
+import React, { useState, useContext} from "react";
+import { GlobalContext } from "../context/GlobalState";
+import cuid from 'cuid';
+
+
 
 const AddTransaction = () => {
   const [text, setText] = useState('')
   const [amount, setAmount] = useState('')
 
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: cuid(),
+      text, 
+      amount: +amount
+    }
+
+    addTransaction(newTransaction);
+
+  }
+
   return (
     <div>
       <h3>Add new transaction</h3>
-      <form id="form">
+      <form onSubmit = {onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value = {text} onChange = {(e) => setText(e.target.value)} placeholder="Enter text..." />
